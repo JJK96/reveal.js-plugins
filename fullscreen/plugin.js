2d1c38ee4b5d4798a47c49df0ed70b9ca579e9b8
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2023-12-27 13:31:26 +0100

    Update for new version of reveal.js

--- fullscreen/plugin.js
+++ fullscreen/plugin.js
@@ -9,38 +9,34 @@
 **
 ******************************************************************/
 
-window.RevealFullscreen = window.RevealFullscreen || {
-    id: 'RevealFullscreen',
-    init: function(deck) {
-        initFullscreen(deck);
+var RevealFullscreen = () => ({
+	id: "RevealFullscreen",
+    init: (Reveal) => {
+    	var config = null;
+    	var ready = false;
+
+    	Reveal.addEventListener( 'ready', function( event ) {
+    		ready = true;
+    		config = { width: Reveal.getConfig().width, height: Reveal.getConfig().height, margin: Reveal.getConfig().margin };
+    		if ( Reveal.getCurrentSlide().hasAttribute("data-fullscreen") ) {
+    			Reveal.configure( { width: window.innerWidth, height: window.innerHeight, margin: 0 } );
+    		}
+    	} );
+
+    	Reveal.addEventListener( 'slidechanged', function( event ) {
+    		if ( Reveal.getCurrentSlide().hasAttribute("data-fullscreen") ) {
+    			Reveal.configure( { width: window.innerWidth, height: window.innerHeight, margin: 0 } );
+    		}
+    		else {
+    			Reveal.configure( config );
+    		}
+    	} );
+
+    	window.addEventListener( 'resize', function( event ) {
+    		if ( ready && Reveal.getCurrentSlide().hasAttribute("data-fullscreen") ) {
+    			Reveal.configure( { width: window.innerWidth, height: window.innerHeight, margin: 0 } );
+    		}
+    	} );
+
     }
-};
-
-const initFullscreen = function(Reveal){
-	var config = null;
-	var ready = false;
-
-	Reveal.addEventListener( 'ready', function( event ) {
-		ready = true;
-		config = { width: Reveal.getConfig().width, height: Reveal.getConfig().height, margin: Reveal.getConfig().margin };
-		if ( Reveal.getCurrentSlide().hasAttribute("data-fullscreen") ) {
-			Reveal.configure( { width: window.innerWidth, height: window.innerHeight, margin: 0 } );
-		}
-	} );
-
-	Reveal.addEventListener( 'slidechanged', function( event ) {
-		if ( Reveal.getCurrentSlide().hasAttribute("data-fullscreen") ) {
-			Reveal.configure( { width: window.innerWidth, height: window.innerHeight, margin: 0 } );
-		}
-		else {
-			Reveal.configure( config );
-		}
-	} );
-
-	window.addEventListener( 'resize', function( event ) {
-		if ( ready && Reveal.getCurrentSlide().hasAttribute("data-fullscreen") ) {
-			Reveal.configure( { width: window.innerWidth, height: window.innerHeight, margin: 0 } );
-		}
-	} );
-
-};
+})
